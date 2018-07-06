1f1ac7efcadea3814bfce92d6eced1602f5b86a5
sarthak jain <jainsarthak.dee@gmail.com>
2018-07-06 16:53:02 +0530

    Changes in settings

---
+++ Web Scrapping/settings.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for Projects project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     https://doc.scrapy.org/en/latest/topics/settings.html
+#     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
+#     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+ 
+BOT_NAME = 'Projects'
+
+SPIDER_MODULES = ['Projects.spiders']
+NEWSPIDER_MODULE = 'Projects.spiders'
+
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'Projects (+http://www.yourdomain.com)'
+LOG_LEVEL = 'ERROR'
+RETRY_TIMES = 2
+CONNECTION_TIMEOUT = 30
+USER_AGENT_LIST = 'user_agents.txt'
+
+# Obey robots.txt rules
+#ROBOTSTXT_OBEY = True
+RANDOMIZE_DOWNLOAD_DELAY = False
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+CONCURRENT_REQUESTS = 50
+CONCURRENT_ITEMS = 200
+
+# Configure a delay for requests for the same website (default: 0)
+# See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
+# See also autothrottle settings and docs
+DOWNLOAD_DELAY = 0
+# The download delay setting will honor only one of:
+CONCURRENT_REQUESTS_PER_DOMAIN = 50
+#CONCURRENT_REQUESTS_PER_IP = 16
+
+# Disable cookies (enabled by default)
+#COOKIES_ENABLED = False
+
+# Disable Telnet Console (enabled by default)
+#TELNETCONSOLE_ENABLED = False
+
+# Override the default request headers:
+#DEFAULT_REQUEST_HEADERS = {
+#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+#   'Accept-Language': 'en',
+#}
+
+# Enable or disable spider middlewares
+# See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+#SPIDER_MIDDLEWARES = {
+#    'Projects.middlewares.ProjectsSpiderMiddleware': 543,
+#}
+
+# Enable or disable downloader middlewares
+# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
+DOWNLOADER_MIDDLEWARES = {
+#    'Projects.middlewares.ProjectsDownloaderMiddleware': 543,
+     #'random_useragent.RandomUserAgentMiddleware': 400,
+     #'com.crawler.scraper.middlewares.proxy_middleware.ProxyMiddleware': 410,
+     'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+ }
+
+# Enable or disable extensions
+# See https://doc.scrapy.org/en/latest/topics/extensions.html
+#EXTENSIONS = {
+#    'scrapy.extensions.telnet.TelnetConsole': None,
+#}
+
+# Configure item pipelines
+# See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+#ITEM_PIPELINES = {
+#    'Projects.pipelines.ProjectsPipeline': 300,
+ #    'com.crawler.scraper.pipelines.generic_pipeline.Pipeline': 300
+#}
+
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See https://doc.scrapy.org/en/latest/topics/autothrottle.html
+#AUTOTHROTTLE_ENABLED = True
+# The initial download delay
+#AUTOTHROTTLE_START_DELAY = 5
+# The maximum download delay to be set in case of high latencies
+#AUTOTHROTTLE_MAX_DELAY = 60
+# The average number of requests Scrapy should be sending in parallel to
+# each remote server
+#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# Enable showing throttling stats for every response received:
+#AUTOTHROTTLE_DEBUG = False
+
+# Enable and configure HTTP caching (disabled by default)
+# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+#HTTPCACHE_ENABLED = True
+#HTTPCACHE_EXPIRATION_SECS = 60*60*24
+#HTTPCACHE_DIR = 'httpcache'
+#HTTPCACHE_IGNORE_HTTP_CODES = []
+#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
